009ae47591d39c260a5d06a60398f7a86b4c2aab
Luana Lima <luanalima.2006@hotmail.com>
2024-10-10 13:37:04 -0300

    second version of JS

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Aparecer o total mesmo sem a porcentagem ser inserida
-// Organizar o código (variáveis, funções, etc)
-// Adicionar comentários ✅
-// Melhorar as funções
-// Diminuir a repetição de código
 // Alterar algumas regras de estilização
-
 
 const bill = document.querySelector("#bill");
 const botoes = document.querySelectorAll(".botao");
@@ -13,85 +7,84 @@ const qtdPessoas = document.querySelector("#people");
 const invalidMessage = document.querySelector("#invalidMessage");
 let tip = document.querySelector("#tip");
 let total = document.querySelector("#total");
-const reset = document.querySelector("#reset");
+const resetButton = document.querySelector("#reset");
 
-// Mostra a mensagem de erro caso qtdPessoas <= 0
+// Mostra a mensagem de erro caso qtdPessoas > 0
 qtdPessoas.addEventListener("input", () => {
-  // Se a quantidade de pessoas for < 1 faz o display de uma mensagem de erro
-  if (qtdPessoas.value <= 0) {
-    invalidMessage.style.display = "block";
-    qtdPessoas.style.outline = "2px solid rgba(255, 0, 0, 0.8)";
-  } else { // Caso contrário a mensagem de erro se mantém oculta
-    invalidMessage.style.display = "none";
-    qtdPessoas.style.outline = "";
-  }
+  const validacao = qtdPessoas.value > 0;
+  // Mostra a mensagem de erro
+  !validacao
+    ? (invalidMessage.style.display = "block")
+    : (invalidMessage.style.display = "none");
+  // Muda a estilização do input
+  !validacao
+    ? (qtdPessoas.style.outline = "2px solid rgba(255, 0, 0, 0.8)")
+    : (qtdPessoas.style.outline = "");
 });
 
 // Calcula o resultado final e coloca no HTML
-result = () => {
-  // Coleta a porcentagem selecionada na função selecionarBotao()
+function result() {
+  // Testa se existe uma tag com a classe seleiconado, e tenta pegar o value da porcentagem, o conteúdo da tag ou o valor do input.
   let porcentagem = document.querySelector(".selecionado");
+  porcentagem =
+    porcentagem?.value ||
+    porcentagem?.textContent.slice(0, -1) ||
+    customPercentage.value;
 
-  //Se a porcentagem tiver um value (ou seja, estiver um input), pega o value
-  if (porcentagem.value) {
-    porcentagem = porcentagem.value;
-  } else { // caso contrário, pega o texto dentro da tag do começo até o penúltimo digito da tag = slice(0/começo, -1/ignora o último digito)
-    porcentagem = porcentagem.outerText.slice(0, -1);
-  }
-
-  // se a quantidade de pessoas for > 0...
+  // Se a qtdPessoas > 0, faz o display do valor a ser pago
   if (qtdPessoas.value > 0) {
-    // pega os valores, converte pra numero, faz a equação e arredonda pra decimal 
-    tip.innerHTML =
-      "$ " +
-      ((+porcentagem * +bill.value) / +qtdPessoas.value / 100).toFixed(2);
-    // pega o valor da gorjeta, soma com a conta e divide pra cada pessoa arredondando pra decimal 
-    total.innerHTML =
-      "$ " +
-      ((+tip.outerText.slice(1) + +bill.value) / qtdPessoas.value).toFixed(2);
-  } else { // caso contrário, mantem os valores zerados
-    tip.innerHTML = "$ 0.00";
-    total.innerHTML = "$ 0.00";
-  }
-};
+    tip.textContent = `$ ${(
+      (+porcentagem * +bill.value) /
+      +qtdPessoas.value /
+      100
+    ).toFixed(2)}`;
 
-//Coletar o botão selecionado
-function selecionarBotao() {
-  for (let i = 0; i < botoes.length; i++) { // vai em cada botão e tenta escutar um click, caso escute
-    botoes[i].addEventListener("click", () => {
-      for (let j = 0; j < botoes.length; j++) { // percorre os botões dnv
-        if (botoes[j] == botoes[i]) {  // se o botão clicado e o botão percorrido forem iguais, ele adiciona a tag selecionado
-          botoes[j].classList.add("selecionado");
-        } else { // se não, ele remove a classe selecionado
-          botoes[j].classList.remove("selecionado");
-        }
-      }
-      result(); // e chama a função pra fazer o calculo
-    });
+    total.textContent = `$ ${(
+      (+tip.textContent.slice(1) + +bill.value) /
+      +qtdPessoas.value
+    ).toFixed(2)}`;
+  } else {
+    // Se não, mantém zerado
+    tip.textContent = "$ 0.00";
+    total.textContent = "$ 0.00";
   }
 }
 
-// para pra escutar por mudanças na calculadora
-bill.addEventListener("input", result);
-qtdPessoas.addEventListener("input", result);
-customPercentage.addEventListener("input", result);
-selecionarBotao();
+//Coleta o botão selecionado
+botoes.forEach((botao) => {
+  // vai em cada botão e tenta escutar um click, caso escute
+  botao.addEventListener("click", () => {
+    botoes.forEach((b) => b.classList.remove("selecionado"));
 
-//Reseta o site respectivamente: porcentagem customizada, conta, qtd de Pessoas, calculo da gorjeta e do total, algumas estilizações e os botões selecionados.
-reset.addEventListener("click", () => { 
+    botao.classList.add("selecionado");
+
+    customPercentage.value = ""; // Se um botao for selecionado, limpa a porcentagem do input
+
+    result(); // e chama a função pra fazer o calculo
+  });
+});
+
+// Reseta a calculadora para o padrão
+function reset() {
+  // Reseta os valores dos inputs
   customPercentage.value = "";
   bill.value = "";
   qtdPessoas.value = 1;
-  tip.innerHTML = "$ 0.00";
-  total.innerHTML = "$ 0.00";
 
-  customPercentage.style.outline = " ";
+  // Reseta os valores exibidos
+  tip.textContent = "$ 0.00";
+  total.textContent = "$ 0.00";
 
+  // Oculta mensagem de erro e limpa estilização
   invalidMessage.style.display = "none";
   qtdPessoas.style.outline = "";
-  for (let i = 0; i < botoes.length; i++) {
-    if (botoes[i].classList.contains("selecionado")) {
-      botoes[i].classList.remove("selecionado");
-    }
-  }
-});
+
+  // Remove a classe 'selecionado' de qualquer botão ativo
+  botoes.forEach((botao) => botao.classList.remove("selecionado"));
+}
+
+//Procura por mudanças na calculadora
+bill.addEventListener("input", result);
+qtdPessoas.addEventListener("input", result);
+customPercentage.addEventListener("input", result);
+resetButton.addEventListener("click", reset);
